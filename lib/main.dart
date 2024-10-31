3d376c18a607cc6a812aca69e28961561b83a5c0
Franklin <franloclounch2@gmail.com>
2024-10-30 22:09:49 -0500

    Boton

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MiAplicacion());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MiAplicacion extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          title: Text('Mi Aplicación'),
+        ),
         body: Center(
-          child: Text('Hello World!'),
+          child: ElevatedButton(
+            onPressed: () {
+// Acción a realizar cuando se presiona el botón
+              print('¡Botón presionado!');
+            },
+            child: Text('Presioname ahora'),
+          ),
         ),
       ),
     );
