a84645e4999dca6107503fabcef602c8a7e51ed4
Franklin <franloclounch2@gmail.com>
2024-10-31 06:38:37 -0500

    S5 ejercicio 13

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,28 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+      title: "Columnas",
+      home: Inicio(),
+    );
+  }
+}
+
+class Inicio extends StatefulWidget {
+  const Inicio({super.key});
+
+  @override
+  State<Inicio> createState() => _InicioState();
+}
+
+class _InicioState extends State<Inicio> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Mis columnas"),
+      ),
+      body: Center(
+        child: Text("Las columnas"),
       ),
     );
   }
