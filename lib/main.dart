a24067bfc26d23ba3b9c2fe46aeade50b2c6675f
Franklin <franloclounch2@gmail.com>
2024-10-30 22:36:27 -0500

    ElevateBotton customerxx

--- lib/main.dart
+++ lib/main.dart
@@ -10,15 +10,38 @@ class MiAplicacion extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Mi Aplicación'),
+          title: const Text('Mi Aplicación'),
         ),
         body: Center(
-          child: ElevatedButton(
-            onPressed: () {
-// Acción a realizar cuando se presiona el botón
-              print('¡Botón presionado!');
-            },
-            child: Text('Presioname ahora'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  // Acción a realizar cuando se presiona el botón
+                  print('¡Botón presionado!');
+                },
+                child: const Text('Presioname Franklin De La Cruz Asto'),
+              ),
+              const SizedBox(height: 20), // Espacio entre los botones
+              ElevatedButton(
+                onPressed: () {
+                  // Acción a realizar cuando se presiona el botón
+                  print(
+                      '¡Botón personalizado presionado!'); // Acción para el nuevo botón
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor:
+                      Colors.blue, // Cambia 'primary' por 'backgroundColor'
+                  foregroundColor:
+                      Colors.white, // Cambia 'onPrimary' por 'foregroundColor'
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+                child: const Text('Botón personalizado'),
+              ),
+            ],
           ),
         ),
       ),
