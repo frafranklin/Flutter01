6f5e51c8f9744156251826ed42d547c3651177d9
Franklin <franloclounch2@gmail.com>
2024-10-31 10:51:39 -0500

    Botón personalizado

--- lib/main.dart
+++ lib/main.dart
@@ -1,81 +1,66 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(TarjetaPersonalizadaApp());
+void main() => runApp(BotonPersonalizadoApp());
 
-class TarjetaPersonalizadaApp extends StatelessWidget {
+class BotonPersonalizadoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Tarjeta Personalizada',
+      title: 'Botón Personalizado',
       theme: ThemeData(
-        primarySwatch: Colors.teal,
+        primarySwatch: Colors.blue,
       ),
-      home: TarjetaPersonalizadaPage(),
+      home: BotonPersonalizadoPage(),
     );
   }
 }
 
-class TarjetaPersonalizadaPage extends StatelessWidget {
+class BotonPersonalizadoPage extends StatelessWidget {
+// Función que se ejecuta al presionar el botón
+  void _accionBoton(BuildContext context) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('¡Botón Presionado!'),
+        duration: Duration(seconds: 2),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Tarjeta Personalizada Franklin'),
+        title: Text('Botón Personalizado'),
         centerTitle: true,
       ),
       body: Center(
-        child: Container(
-          width: 300.0,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(16.0),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black26,
-                blurRadius: 10.0,
-                offset: Offset(0, 5),
-              ),
-            ],
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-// Imagen superior con esquinas redondeadas
-              ClipRRect(
-                borderRadius: BorderRadius.vertical(top: Radius.circular(16.0)),
-                child: Image.network(
-                  'https://via.placeholder.com/300x150.png?text=Imagen+de+Ejemplo',
-                  height: 150.0,
-                  width: double.infinity,
-                  fit: BoxFit.cover,
+        child: GestureDetector(
+          onTap: () => _accionBoton(context),
+          child: Container(
+            width: 200.0,
+            padding: EdgeInsets.symmetric(vertical: 15.0),
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              borderRadius: BorderRadius.circular(30.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.blueAccent.withOpacity(0.6),
+                  spreadRadius: 2,
+                  blurRadius: 7,
+                  offset: Offset(0, 3),
+                ),
+              ],
+            ),
+            child: Center(
+              child: Text(
+                'Presióname Franklin ',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-// Título de la tarjeta
-              Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Text(
-                  'Título de la Tarjeta',
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.teal[800],
-                  ),
-                ),
-              ),
-// Descripción de la tarjeta
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 16.0),
-                child: Text(
-                  'Hola que tal soy FRANKLIN DE LA CRUZ ASTO Y ME GUSTA LA PROGRAMACIÓN ORIENTADA A OBJETOS, HE APRENDIDO VARIOS LENGUAJES DE PROGRAMACIÓN',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    color: Colors.grey[700],
-                  ),
-                ),
-              ),
-              SizedBox(height: 16.0),
-            ],
+            ),
           ),
         ),
       ),
