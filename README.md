b3aaca32cb4dad2af7da2150f32f90cdd7c06031
Franklin <franloclounch2@gmail.com>
2024-10-31 12:26:41 -0500

    Container

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_s7
+
+A new Flutter project.
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyApp',
+      home: Inicio(),
+    );
+  }
+}
+
+class Inicio extends StatefulWidget {
+  const Inicio({super.key});
+
+  @override
+  State<Inicio> createState() => _InicioState();
+}
+
+class _InicioState extends State<Inicio> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Bienvenidos"),
+      ),
+      body: Center(
+        child: Text("Franklin De La Cruz Asto"),
+      ),
+    );
+  }
+}
